bee26c302474f85899dcc253f49bbfd70b2c1db1
dengdeng <1666490711@qq.com>
2018-09-03 09:50:12 +0800

    动态获取用户内容

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var bysass = require('gulp-sass')
 var yscss = require('gulp-clean-css')
 var server = require('gulp-webserver')
 var ysjs = require('gulp-uglify')
+var fs = require('fs')
+var path = require('path')
+var url = require('url')
 console.log(ysjs)
     //压缩css
 gulp.task('yscss', function() {
@@ -28,9 +31,22 @@ gulp.task('server', function() {
         .pipe(server({
             port: 8888,
             middleware: function(req, res, next) {
-                console.log(req.url)
+                var pathname = url.parse(req.url).pathname
+
+                if (pathname == '/favicon.ico') {
+                    return res.end()
+                }
+                //动态内容
+                if (pathname == '/api/first') {
+                    var nr = url.parse(req.url, true).query.value
+                    console.log(nr)
+                    res.end(JSON.stringify({ code: 0, msg: cs }))
+                } else {
+                    pathname = pathname == '/' ? 'html/index.html' : pathname
+                    res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)))
+                }
+                res.end('.')
             }
         }))
 })
-
 gulp.task('hb', gulp.series('yscss', 'ysjs', 'server', 'watch'))
